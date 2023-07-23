cc518e05f0cde907d70bc38a838054cf2b010648
sathwik <sathwikacharya61@gmail.com>
2023-07-23 19:01:38 +0530

    Added files

---
+++ p.py
@@ -0,0 +1,50 @@
+####### 1nd program
+# val=input("Enter a number")
+
+# if(val==val[::-1]):
+#     print("Number is palindrome")
+# else:
+#     print("Number is not palindrome")
+
+# for i in range(10):
+#     if val.count(str(i))>0:
+#         print(str(i),"Appears ", val.count(str(i))," Times")
+
+
+
+
+# m1=int(input("Enter the  m1 Marks"))
+# m2=int(input("Enter the  m2 Marks"))
+# m3=int(input("Enter the  m3 Marks"))
+
+# if m1>m2:
+#     if m2>m3:
+#         tot=m1+m2
+#     else:
+#         tot=m1+m3
+# elif m1>m3:
+#      tot=m2+m1
+# else:
+#     tot=m2+m3
+# print("Total = ",tot)
+
+
+####### 2nd program
+
+# def fn(n):
+#     if n<=0:
+#         print("Invalid input")
+#     elif n==1:
+#         return 0
+#     elif n==2:
+#         return 1
+    
+#     else:
+#         return fn(n-1)+fn(n-2)
+    
+# n=int(input("Enter a number"))
+# print("Fn(",n,") = ",fn(n), sep="")
+
+# ---
+
+
